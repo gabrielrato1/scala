4341237225831c1e89c71e37d88e94ddafbb8c92
Gabriel <gabrieltf141@gmail.com>
2024-10-04 22:09:00 +0100

    first

---
+++ conversion/FrenchDate.scala
@@ -0,0 +1,11 @@
+import java.time.format.{DateTimeFormatter, FormatStyle}
+import java.time.LocalDate
+import java.util.Locale._
+
+object FrenchDate {
+  def main(args: Array[String]): Unit = {
+    val now = LocalDate.now
+    val df = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(FRANCE)
+    println(df.format(now))
+  }
+}
---
+++ hello/HelloWorld.scala
@@ -0,0 +1,14 @@
+// object defines a singleton object
+/*object HelloWorld {
+  //Unit is similar to Void return
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
+*/
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
